93bb12cde3dd5cdeced6e16c9cb734ae24fafbaa
Joost1982 <38584259+Joost1982@users.noreply.github.com>
2019-10-10 22:02:30 +0200

    'xamarin'

---
+++ Csharp rommel/HeadFirst/hoofdstuk 6/ConsoleAppOverrideVirtualExample/ConsoleAppOverrideVirtualExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOverrideVirtualExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Owner owner = new Owner();
+            Safe safe = new Safe();
+            JewelThief jewelthief = new JewelThief();
+            jewelthief.OpenSafe(safe, owner);
+
+            Console.ReadKey(); // om te voorkomen dat het venster meteen sluit
+
+        }
+    }
+}
